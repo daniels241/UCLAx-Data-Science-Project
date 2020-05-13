278a925d4dc720b4d3da943c326b97d3e2703fe9
Evan Davis-Palley <wowccraft@gmail.com>
2020-05-12 21:51:01 -0700

    Added new interfaces and optimized constants

--- website/src/app/graph/image.ts
+++ website/src/app/graph/image.ts
@@ -1,22 +1,48 @@
 export interface Image {
-    source: string;
-    param1?: Option;
-    param2?: Option;
+    source: string,
+    param1?: Option,
+    param2?: Option,
 }
 
-export var graphs: [{ title?: string, img?: Image; }] = [
+export interface Graph { 
+    title?: string, 
+    img?: Image,
+    desc?: string,
+}
+
+export interface Table {
+    title?: string, 
+    source: string,
+    desc?: string,
+}
+
+export const graphs: Graph[] = [
     {
         title: "First Time Homeless vs Year",
         img: {
             source: "../../assets/SPM5_x_year.png"
-        }
+        },
+        desc: `Bullet points go here!`
+    },
+    {
+        title: "First Time Homeless vs Year",
+        img: {
+            source: "../../assets/SPM5_x_year.png"
+        },
+        desc: `Bullet points go here!`
     },
 ]
 
-export var tables: [{ title?: string, source?: string; }] = [
+export const tables: Table[] = [
     {
         title: "First Time Homeless vs Year",
-        source: ""
+        source: "",
+        desc: `Bullet points go here!`
+    },
+    {
+        title: "First Time Homeless vs Year",
+        source: "",
+        desc: `Bullet points go here!`
     },
 ]
 
