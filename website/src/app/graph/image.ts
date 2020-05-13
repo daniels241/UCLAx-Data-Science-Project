df70a8e087df9342eae712a7021d5c9d386eeabb
Evan Davis-Palley <wowccraft@gmail.com>
2020-05-12 21:41:27 -0700

    Added graphs & tables arrays

--- website/src/app/graph/image.ts
+++ website/src/app/graph/image.ts
@@ -1,8 +1,24 @@
 export interface Image {
     source: string;
-    param1: Option;
-    param2: Option;
+    param1?: Option;
+    param2?: Option;
 }
+
+export var graphs: [{ title?: string, img?: Image; }] = [
+    {
+        title: "First Time Homeless vs Year",
+        img: {
+            source: "../../assets/SPM5_x_year.png"
+        }
+    },
+]
+
+export var tables: [{ title?: string, source?: string; }] = [
+    {
+        title: "First Time Homeless vs Year",
+        source: ""
+    },
+]
 
 export interface Option {
     name: string;
